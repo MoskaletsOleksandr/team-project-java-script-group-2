012428b77123f13e5737bba86aabb5fc0c8d150b
IhorPinchuk <pinchukkk@ukr.net>
2023-04-14 19:00:09 +0300

    btn-up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //імпортуємо бібліотеки та інші файли
 import { fetchTrendMoves } from './js/api';
 import { createTrendMovesMarkup } from './js/createMarkup';
-//
+import throttle from 'lodash.throttle'; // npm i lodash.throttle
 //
 //
 //
@@ -27,10 +27,10 @@ const refs = {
   addToQueueBtn: document.querySelector('.add-to-queue-btn'),
   removeFromWatchedBtn: document.querySelector('.remove-from-watched-btn'),
   removeFromQueueBtn: document.querySelector('.remove-from-queue-btn'),
+  btnUpEl: document.querySelector('.btn-up'),
 };
 
 console.log(refs);
-//
 //
 //
 //
@@ -250,33 +250,33 @@ console.log(refs);
 //Ігор
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+refs.btnUpEl.addEventListener('click', scrollUp);
+
+function show() {
+  refs.btnUpEl.classList.remove('btn-up_hide');
+}
+
+function hide() {
+  refs.btnUpEl.classList.add('btn-up_hide');
+}
+
+window.addEventListener(
+  'scroll',
+  throttle(() => {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+    scrollY > 400 ? show() : hide();
+  }, 500)
+);
+
+function scrollUp() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+}
 //
 //
 //
