9d3e45e69b421e163ad32890bbbf0c157537cb32
Олександр <moskaletzsanych@gmail.com>
2023-04-17 16:20:13 +0300

    Update localStorage.js

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,11 +1,16 @@
 export function saveLocalStorage(key, value) {
-  try {localStorage.setItem(key, JSON.stringify(value));}
-  catch {console.log(error);}
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch {
+    console.log(error);
+  }
 }
 // функція повертає інформацію про обєкт із сховища
-export  function loadLocalStorage(key) {
-  try {const get = localStorage.getItem(key);
-    return parse = JSON.parse(get);}
-  catch {console.log(error);}
+export function loadLocalStorage(key) {
+  try {
+    const get = localStorage.getItem(key);
+    return (parse = JSON.parse(get));
+  } catch (error) {
+    console.log(error);
+  }
 }
-
