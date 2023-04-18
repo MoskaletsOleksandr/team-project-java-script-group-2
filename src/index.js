97a90e434d385189b7edbbc6d6740cdb4bc73d5b
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-04-18 18:43:37 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1022,6 +1022,7 @@ function onCloseMovieModal(e) {
       return;
     }
     movieIdForModalMarkup = event.target.parentElement.dataset.id;
+    console.log(movieIdForModalMarkup);
   return;
 }
 
@@ -1041,7 +1042,9 @@ export function handleMovieCard(event) {
       refs.movieModalEl.classList.remove('is-hidden');
       refs.backdropMovieModal.addEventListener('click', onCloseMovieModal);
       window.addEventListener('keydown', onCloseMovieModal);
+
       const markup = createMoveModalMarkup(data, movieIdForModalMarkup);
+
       refs.movieModalFilmInfoEl.innerHTML = markup;
       refs.bodyEl.style.overflow = 'hidden';
       return data;
